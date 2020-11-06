e09d8ea26393e8fc1302de1d4feaae133e031989
Kapil <sahu.kapil1811@outlook.com>
2020-11-06 11:49:52 +0530

    Created Dao interfaces for each entity

---
+++ src/main/java/com/upgrad/hirewheels/dao/UsersDao.java
@@ -0,0 +1,13 @@
+package com.upgrad.hirewheels.dao;
+
+import com.upgrad.hirewheels.entities.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface UsersDao extends JpaRepository<Users, Integer> {
+
+    public List<Users> findByFirstNameIgnoreCase(String firstName);
+    public List<Users> findByFirstNameOrLastNameIgnoreCase(String firstName, String lastName);
+    public List<Users> findByEmailIgnoreCase (String email);
+    public List<Users> findByMAndMobileNoIgnoreCase(char mobileNo);
+}
